88e656e1351ca1bdb993d6bac95db3d959499e71
Gayoung Park <gayoungp@uw.edu>
2022-03-27 22:22:04 -0700

    changed cv figures for sharma intervals

--- analysis/paper/002-coefficient-variation.R
+++ analysis/paper/002-coefficient-variation.R
@@ -75,7 +75,7 @@ ggsave(here::here("analysis/figures/003-cv-sharma.png"),
 #Make a table of CVs for all variable grouped by site
 cv_by_site_df  <-
   df_full_sitename %>%
-  select(-SPstage.Stage, -lat_dd, -long_dd, -sitename) %>%
+  select(-SPstage.Stage, -lat_dd, -long_dd, -sitename, -SPstage.Raw_material) %>%
   group_by(full_sitename) %>%
   add_tally() %>%
   filter( n > 1) %>%
@@ -87,17 +87,6 @@ cv_by_site_df  <-
   summarise(cv_by_site =   sharma_cv(value),
             cv_low_sharma =  sharma_int_low(value),
             cv_high_sharma = sharma_int_high(value))
-
-cv_by_site_df %>%
-  ggplot(aes(x = variable, y = cv_by_site)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = cv_low_sharma,
-                    ymax = cv_high_sharma), width = .2) +
-  xlab("Site") +
-  ylab("Coefficient of Variation on Attributes") +
-  facet_wrap( ~ full_sitename, scales = "free_y") +
-  theme(legend.position = "none") +
-  coord_cartesian(ylim = c(0,100))
 
 
 ## CV for per each site (n=X) with the full site name : label
@@ -115,43 +104,38 @@ cv_by_full_site_df_label <-
 
 ## add label to main dataframe
 cv_plot_site_label <- cv_by_site_df %>%
- left_join (cv_by_full_site_df_label) %>%
+  left_join (cv_by_full_site_df_label) %>%
   select (- data,-full_sitename)
 
-# facetted bar plot per each site, drop legend
-site_bar_plot <- cv_plot_site_label %>%
-  pivot_longer(cols = -label,
-               names_to = "group") %>%
-  ggplot(aes(x = group, y = value, fill = label)) +
-  geom_col() +
-  xlab("Site") +
-  ylab("Coefficient of Variation on Attributes") +
-  facet_wrap( ~ label) +
-  theme(legend.position = "none")
+## facet plot
 
-site_bar_plot <- cv_plot_site_label %>%
-  pivot_longer(cols = -label,
-               names_to = "group") %>%
-  ggplot(aes(x = group, y = value, fill = label)) +
+cv_plot_site_label %>%
+  ggplot(aes(x = variable, y = cv_by_site)) +
   geom_point() +
-  geom_errorbar(aes(ymin = min(cv_plot_site_label$group), ymax = max(cv_plot_site_label$group)), width = .2) +
+  geom_errorbar(aes(ymin = cv_low_sharma,
+                    ymax = cv_high_sharma), width = .2) +
   xlab("Site") +
   ylab("Coefficient of Variation on Attributes") +
-  facet_wrap( ~ label) +
-  theme(legend.position = "none")
+  facet_wrap( ~ label, scales = "free_y") +
+  theme(legend.position = "none") +
+  coord_cartesian(ylim = c(0,700)) +
+  theme_bw()
 
-# ggplot(cv_data_sharma_named, aes(attribute, all_cv_sharma)) +        # ggplot2 plot with confidence intervals
-#   geom_point() +
-#   geom_errorbar(aes(ymin = all_low_sharma, ymax = all_high_sharma), width = .2) +
-#   ylab("CV") +
-#   xlab("Variables") +
-#   geom_text(aes(label = round(all_cv_sharma,1)), col="blue", hjust = -0.3, size = 3) +
-#   theme_bw()
-# facetted bar plot with (n=X)
-#site_bar_plot + scale_fill_discrete(name = "Site name", labels = cv_by_full_site_df_label$label)
+# checking SC site values
+# Sac <- df_full_sitename %>%
+#   filter(full_sitename=="Sachang")
+#
+# cv(Sac$MW)
+# sharma_int_low(Sac$MW)
+# sharma_int_high(Sac$MW)
+#
+# cv(Sac$SL)
+# sharma_int_low(Sac$SL)
+# sharma_int_high(Sac$SL)
+
 
 ggsave(here::here("analysis/figures/004-cv-sites.png"),
-       width = 7,
+       width = 8.5,
        height = 4.5,
        units = "in")
 
@@ -162,114 +146,111 @@ ggsave(here::here("analysis/figures/004-cv-sites.png"),
 # Exclude the first phase that has only two artefacts
 cv_by_stage_df  <-
   df_sitename %>%
-  select(-sitename) %>%
+  select(-sitename, -SPstage.Raw_material) %>%
   group_by(SPstage.Stage) %>%
   add_tally() %>%
   filter(SPstage.Stage > 1) %>%
-  nest(-SPstage.Stage) %>%
-  mutate(cv_by_stage = map(data, ~map_df(.x, cv)))
+  select(-n) %>%
+  pivot_longer(-SPstage.Stage,
+               names_to = "variable",
+               values_to = "value") %>%
+  group_by(SPstage.Stage, variable) %>%
+  summarise(cv_by_site =   sharma_cv(value),
+            cv_low_sharma =  sharma_int_low(value),
+            cv_high_sharma = sharma_int_high(value))
 
-cv_by_stage_df_unnest <-
-  cv_by_stage_df %>%
-  unnest(cv_by_stage)
-
-cv_plot_stage <- cv_by_stage_df_unnest %>%
-  select(-data, -n)
+# facet plot for two phases
+cv_by_stage_df %>%
+  ggplot(aes(x = factor(SPstage.Stage), y = cv_by_site, colour = factor(SPstage.Stage))) +
+  geom_point() +
+  geom_errorbar(aes(ymin = cv_low_sharma,
+                    ymax = cv_high_sharma), width = .05) +
+  xlab("Attributes") +
+  ylab("Coefficient of Variation on Attributes") +
+  facet_wrap( ~ variable, scales = "free_y") +
+  coord_cartesian(ylim = c(15,35)) +
+  theme_bw() +
+  labs(color = "Phase")
 
 # bar plot for two phases
-cv_plot_stage %>%
-  pivot_longer(cols = -SPstage.Stage, names_to = "group") %>%
-  ggplot(aes(x= SPstage.Stage, y = value, fill = as.factor(SPstage.Stage))) +
-  geom_bar(stat="identity", position=position_dodge()) + labs(x="Attributes", fill= "Phase")+
-  facet_wrap( ~ group) +
-  theme_bw()
+# cv_plot_stage %>%
+#   select(-SPstage.Raw_material) %>%
+#   pivot_longer(cols = -SPstage.Stage, names_to = "group") %>%
+#   ggplot(aes(x= SPstage.Stage, y = value, fill = as.factor(SPstage.Stage))) +
+#   geom_bar(stat="identity", position=position_dodge()) + labs(x="Attributes", fill= "Phase")+
+#   facet_wrap( ~ group) +
+#   theme_bw()
 
-ggsave(here::here("analysis/figures/004-cv-phases.png"),
-       width = 5,
+ggsave(here::here("analysis/figures/005-cv-phases.png"),
+       width = 4.5,
        height = 4.5,
        units = "in")
 
-# bar plot for each attribute
-cv_plot_stage %>%
-  ggplot(aes(x=SPstage.Stage, y=ML))+
-  geom_line()
 
 
+#### plot CVs by site with number of artefacts showing >5
 
-#### plot CVs by site with number of artefacts showing >10
-
-cv_by_site_df_label_more_than_10 <-
+cv_by_site_df_label_more_than_5 <-
   df_sitename %>%
   unite(col = "site_phase",
         c(sitename,
           SPstage.Stage),
         sep = "_") %>%
+  select(-SPstage.Raw_material) %>%
   group_by(site_phase) %>%
+  # mutate(label = as.factor(paste0(site_phase,' (N = ', n, ")")))
   add_tally() %>%
   filter( n > 5) %>%
-  mutate(label = as.factor(paste0(site_phase,' (N = ', n, ")"))) %>%   select(-n) %>%
-  nest(-site_phase, -label) %>%
-  mutate(cv_by_site = map(data, ~map_df(.x, cv))) %>%
-  unnest(cv_by_site) %>%
-  select(-data)
+  select(-n) %>%
+  pivot_longer(-site_phase,
+               names_to = "variable",
+               values_to = "value") %>%
+  group_by(site_phase, variable) %>%
+  summarise(cv_by_site =   sharma_cv(value),
+            cv_low_sharma =  sharma_int_low(value),
+            cv_high_sharma = sharma_int_high(value)) %>%
+  filter(variable %in% c("ML", "BL","MW", "TW"))
 
-# bar plot for four assemblages, all attributes
-cv_ten_att <-
-cv_by_site_df_label_more_than_10 %>%
-  pivot_longer(cols = -c(site_phase, label),
-               names_to = "lithic_attribute",
-               values_to = "CV") %>%
-  ggplot(aes(x = lithic_attribute,
-             y = CV)) +
-  geom_col() +
-  facet_wrap(~  label) +
+
+
+cv_by_site_df_label_more_than_5 %>%
+  ggplot(aes(x = factor(variable), y = cv_by_site, colour = factor(variable))) +
+  geom_point() +
+  geom_errorbar(aes(ymin = cv_low_sharma,
+                    ymax = cv_high_sharma), width = .2) +
+  xlab("Asseblages") +
+  ylab("Coefficient of Variation on Attributes") +
+  facet_wrap( ~ site_phase, scales = "free_y") +
+  coord_cartesian(ylim = c(15,60)) +
+  theme_bw() +
+  labs(color = "Attributes")+
   ggtitle("CVs of point attributes by site (with sites >=7 artifacts)")
 
-cv_by_site_df_label_more_than_10_ml_mw_tw <-
-  cv_by_site_df_label_more_than_10 %>%
-  pivot_longer(cols = -c(site_phase, label),
-               names_to = "lithic_attribute",
-               values_to = "CV") %>%
-  filter(lithic_attribute %in% c("BL", "ML", "MW", "TW"))  %>%
-  mutate(phase = parse_number(str_extract(site_phase, "_."))) %>%
-  group_by(site_phase) %>%
-  mutate(mean_cv = mean(CV))
+
+# #### plot CVs by site with number of artefacts showing >=7
+#
+# cv_by_site_df_label_more_than_5_ml_mw_bl <-
+#   cv_by_site_df_label_more_than_5 %>%
+#   pivot_longer(cols = -c(site_phase, label),
+#                names_to = "lithic_attribute",
+#                values_to = "CV") %>%
+#   filter(lithic_attribute %in% c("ML", "BL","MW", "TW"))  %>%
+#   mutate(phase = parse_number(str_extract(site_phase, "_."))) %>%
+#   group_by(site_phase) %>%
+#   mutate(mean_cv = mean(CV))
 
 # bar plot for four assemblages, four attributes
-cv_ten_four_att <-
-  ggplot(cv_by_site_df_label_more_than_10_ml_mw_tw,
-       aes(x = lithic_attribute,
-           y = CV)) +
-  geom_col() +
-  geom_hline(aes(yintercept = mean_cv),
-             colour = "red") +
-  facet_wrap(~  label) +
-  ggtitle("CVs of point attributes by site (with sites >=7 artifacts)") +
-  theme_minimal()
+# ggplot(cv_by_site_df_label_more_than_5_ml_mw_bl,
+#        aes(x = lithic_attribute,
+#            y = CV)) +
+#   geom_col() +
+#   geom_hline(aes(yintercept = mean_cv),
+#              colour = "red") +
+#   facet_wrap(~  label) +
+#   ggtitle("CVs of point attributes by site (with sites >=7 artifacts)") +
+#   theme_minimal()
 
-#### plot CVs by site with number of artefacts showing >=7
-
-cv_by_site_df_label_more_than_10_ml_mw_bl <-
-  cv_by_site_df_label_more_than_10 %>%
-  pivot_longer(cols = -c(site_phase, label),
-               names_to = "lithic_attribute",
-               values_to = "CV") %>%
-  filter(lithic_attribute %in% c("ML", "BL","MW", "TW"))  %>%
-  mutate(phase = parse_number(str_extract(site_phase, "_."))) %>%
-  group_by(site_phase) %>%
-  mutate(mean_cv = mean(CV))
-
-# bar plot for four assemblages, four attributes
-ggplot(cv_by_site_df_label_more_than_10_ml_mw_bl,
-       aes(x = lithic_attribute,
-           y = CV)) +
-  geom_col() +
-  geom_hline(aes(yintercept = mean_cv),
-             colour = "red") +
-  facet_wrap(~  label) +
-  ggtitle("CVs of point attributes by site (with sites >=7 artifacts)") +
-  theme_minimal()
-
-library(cowplot)
-plot_grid(cv_ten_att, cv_ten_four_att)
-
+ggsave(here::here("analysis/figures/006-cv-four-assemblage.png"),
+       width = 5,
+       height = 4,
+       units = "in")
